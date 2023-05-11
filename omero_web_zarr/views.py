e8a35a09757c0c8f1086866900e2a1047bc98ecf
William Moore <w.moore@dundee.ac.uk>
2023-05-11 16:56:27 +0100

    Check that source.schema matches request

--- omero_web_zarr/views.py
+++ omero_web_zarr/views.py
@@ -292,6 +292,9 @@ def apps(request, app, url):
     if source is not None and not source.startswith("http"):
         source = request.build_absolute_uri(source)
         new_url = reverse("zarr_app", kwargs={"url": "", "app": app})
+        # in case Django doesn't know .is_secure() and gives http instead of https
+        if not source.startswith(request.scheme + ":"):
+            source = request.scheme + ":" + source.split(":", 1)[1]
         return redirect(new_url + "?source=" + source)
 
     base_urls = {
